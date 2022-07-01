9ce18eaa0d5019875dee6c0193549f688eef957f
hldh214 <hldh214@gmail.com>
2022-07-01 12:16:56 +0900

    feat(main): add infrared_base
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- myhome/app.py
+++ myhome/app.py
@@ -1,3 +1,6 @@
+import functools
+import subprocess
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 
@@ -8,12 +11,24 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
+async def infrared_base(command, param, update: Update, context: ContextTypes.DEFAULT_TYPE):
+    logger.info(f'executing infrared command: {command}')
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f'executing infrared command: {command}')
+    subprocess.call(['termux-infrared-transmit', '-f', param])
+
+
 def main():
     config = get_config()
 
     application = ApplicationBuilder().token(config.get('token')).build()
 
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    application.add_handler(CommandHandler('start', start))
+
+    infrared = config.get('infrared')
+    for each in infrared:
+        application.add_handler(CommandHandler(
+            each['command'],
+            functools.partial(infrared_base, each['command'], each['param'])
+        ))
 
     application.run_polling()
