9ddfad964e08dc5eea9fd63d81b7f96cc376fa01
hldh214 <hldh214@gmail.com>
2022-07-27 14:31:00 +0900

    feat(main): remove unused code
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- myhome/__init__.py
+++ myhome/__init__.py
@@ -1,5 +1,4 @@
 import json
-import logging.handlers
 import pathlib
 import sys
 
@@ -13,16 +12,6 @@ def get_config(path='config.json'):
         return json.load(f)
 
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-
-file_channel = logging.handlers.TimedRotatingFileHandler(
-    project_root.joinpath('builtin_logger.log'),
-    interval=1,
-    when='D',
-    backupCount=1,
-)
-file_channel.setFormatter(formatter)
-
 logger.remove()
-logger.add(project_root.joinpath('loguru.log'), rotation='1 day', retention=2)
+logger.add(project_root.joinpath('loguru.log'))
 logger.add(sys.stdout, colorize=True)
