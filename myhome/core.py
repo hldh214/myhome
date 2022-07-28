8acbc1c141f3d289dc54423e24319eb4fdd0058e
hldh214 <hldh214@gmail.com>
2022-07-28 20:57:38 +0900

    fix(core): detect awaitable functions
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- myhome/core.py
+++ myhome/core.py
@@ -1,3 +1,4 @@
+import inspect
 import subprocess
 
 from myhome import config
@@ -5,7 +6,12 @@ from myhome import config
 
 async def run_group_command(group, logging_function=None):
     if callable(logging_function):
-        await logging_function(f'executing group command: {group.get("command")}')
+        log_message = f'executing group command: {group.get("command")}'
+        if inspect.isawaitable(logging_function):
+            await logging_function(log_message)
+        else:
+            logging_function(log_message)
+
     for each_command in group['commands']:
         infrared = [each for each in config.get('infrared') if each['command'] == each_command][0]
         await run_single_command(infrared, logging_function)
@@ -13,5 +19,10 @@ async def run_group_command(group, logging_function=None):
 
 async def run_single_command(infrared, logging_function=None):
     if callable(logging_function):
-        await logging_function(f'executing single command: {infrared.get("command")}')
+        log_message = f'executing single command: {infrared.get("command")}'
+        if inspect.isawaitable(logging_function):
+            await logging_function(log_message)
+        else:
+            logging_function(log_message)
+
     subprocess.call(['termux-infrared-transmit', '-f', *infrared['params']])
