f3fe6719cf26907316dd7ed21e4528ebdc7ee48b
hldh214 <hldh214@gmail.com>
2023-01-15 22:01:00 +0900

    feat(main): imp mqtt-slave.py: using threading for message processing
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- myhome/mqtt-slave.py
+++ myhome/mqtt-slave.py
@@ -1,3 +1,4 @@
+import threading
 import time
 
 import ping3
@@ -18,29 +19,32 @@ def connect_mqtt():
     return client
 
 
-def subscribe(client: mqtt_client):
-    def on_message(_client, userdata, msg):
+def subscribe(client: mqtt_client.Client):
+    def on_message(_client: mqtt_client.Client, userdata, msg):
         logger.info(f'Received `{msg.payload.decode()}` from `{triggered_topic}` topic, start leave home countdown')
-
-        alive = 0
-        dead = 0
-
-        for _ in range(myhome.config['monitor']['leave_home_count_down_max']):
-            time.sleep(myhome.config['monitor']['interval'])
-            if ping3.ping(myhome.config['monitor']['ip_addr'], timeout=1):
-                alive += 1
-            else:
-                dead += 1
-
-        if alive > dead:
-            logger.info(f'Still home, alive: {alive}, dead: {dead}')
-            return
-
-        logger.info(f'Leave home, execute leave home commands by mqtt, alive: {alive}, dead: {dead}')
-        _client.publish(callback_topic, '{"status": "absent"}')
+        threading.Thread(target=presence_detection, args=(client,)).start()
 
     client.subscribe(triggered_topic)
     client.on_message = on_message
+
+
+def presence_detection(client: mqtt_client.Client):
+    alive = 0
+    dead = 0
+
+    for _ in range(myhome.config['monitor']['leave_home_count_down_max']):
+        time.sleep(myhome.config['monitor']['interval'])
+        if ping3.ping(myhome.config['monitor']['ip_addr'], timeout=1):
+            alive += 1
+        else:
+            dead += 1
+
+    if alive > dead:
+        logger.info(f'Still home, alive: {alive}, dead: {dead}')
+        return
+
+    logger.info(f'Leave home, execute leave home commands by mqtt, alive: {alive}, dead: {dead}')
+    client.publish(callback_topic, '{"status": "absent"}')
 
 
 def run():
