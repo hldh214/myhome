d3d4162260013cd9037e6e5b98790909f5ccc831
hldh214 <hldh214@gmail.com>
2023-01-16 11:01:02 +0900

    feat(cron): rework using asyncio_mqtt
    Signed-off-by: hldh214 <hldh214@gmail.com>

--- myhome/mqtt-slave.py
+++ myhome/mqtt-slave.py
@@ -1,39 +1,27 @@
-import threading
-import time
+import asyncio
 
 import ping3
-from paho.mqtt import client as mqtt_client
+import asyncio_mqtt
 
 import myhome
 from myhome import logger
 
-broker = myhome.config['monitor']['mqtt_broker']
+hostname = myhome.config['monitor']['mqtt_broker']
+username = 'well-known'
+password = 'well-known'
+
 triggered_topic = 'me/motion_sensor'
 callback_topic = 'me/leave_home'
 
-
-def connect_mqtt():
-    client = mqtt_client.Client()
-    client.username_pw_set('well-known', 'well-known')
-    client.connect(broker)
-    return client
+pd_task = None
 
 
-def subscribe(client: mqtt_client.Client):
-    def on_message(_client: mqtt_client.Client, userdata, msg):
-        logger.info(f'Received `{msg.payload.decode()}` from `{triggered_topic}` topic, start leave home countdown')
-        threading.Thread(target=presence_detection, args=(client,)).start()
-
-    client.subscribe(triggered_topic)
-    client.on_message = on_message
-
-
-def presence_detection(client: mqtt_client.Client):
+async def presence_detection(client: asyncio_mqtt.Client):
     alive = 0
     dead = 0
 
     for _ in range(myhome.config['monitor']['leave_home_count_down_max']):
-        time.sleep(myhome.config['monitor']['interval'])
+        await asyncio.sleep(myhome.config['monitor']['interval'])
         if ping3.ping(myhome.config['monitor']['ip_addr'], timeout=1):
             alive += 1
         else:
@@ -44,14 +32,30 @@ def presence_detection(client: mqtt_client.Client):
         return
 
     logger.info(f'Leave home, execute leave home commands by mqtt, alive: {alive}, dead: {dead}')
-    client.publish(callback_topic, '{"status": "absent"}')
+    await client.publish(callback_topic, '{"status": "absent"}')
 
 
-def run():
-    client = connect_mqtt()
-    subscribe(client)
-    client.loop_forever()
+async def main():
+    reconnect_interval = 5
+
+    while True:
+        try:
+            async with asyncio_mqtt.Client(hostname=hostname, username=username, password=password) as client:
+                async with client.messages() as messages:
+                    await client.subscribe(triggered_topic)
+                    async for message in messages:
+                        logger.info(f'Received `{message.payload.decode()}` from `{triggered_topic}` topic.')
+
+                        global pd_task
+                        if pd_task is not None and not pd_task.done():
+                            logger.warning('Cancel previous task due to new message received.')
+                            pd_task.cancel()
+
+                        pd_task = asyncio.create_task(presence_detection(client))
+        except asyncio_mqtt.MqttError as error:
+            logger.error(f'Error "{error}". Reconnecting in {reconnect_interval} seconds.')
+            await asyncio.sleep(reconnect_interval)
 
 
 if __name__ == '__main__':
-    run()
+    asyncio.run(main())
